54ce8cbe088bef73012fced0f177bfeb63072317
HenryMarcos <thangtranmanh9@gmail.com>
2025-12-15 09:36:39 +0700

    Stuff Stuff

--- basic_algorithm.py
+++ basic_algorithm.py
@@ -1,8 +1,2 @@
-def rectangle_area(rec_width, rec_length):
-    return rec_width * rec_length
-
-def play_a_bad_guessing_game():
-    guess = input("What is that cat over there thinking about? ")
-    print("I don't think that " + guess + " is what that cat is thinking about")
-
-play_a_bad_guessing_game()
+a, b = map(int, input().split())
+print(bin(a ^ b).count('1'))
