c731c357bc94e2180a2681d8ab294515e543cf4c
HenryMarcos <thangtranmanh9@gmail.com>
2025-12-22 10:48:24 +0700

    e

--- basic_algorithm.py
+++ basic_algorithm.py
@@ -1,2 +1,12 @@
-a, b = map(int, input().split())
-print(bin(a ^ b).count('1'))
+d, m, y = map(int, input().split())
+d_list = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0): d_list[2] = 29
+if m > 12 or d_list[m] < d: print("Invalid date")
+else:
+    if d_list[m] < d + 1:
+        d = 1
+        m += 1
+        if m > 12:
+            y += 1
+            m = 1
+    print(f"{d}/{m}/{y}")
---
+++ eep.cpp
@@ -0,0 +1,2 @@
+#include<bits/stdc++.h>
+using namespace std;int main() {int d, m, y;cin >> d >> m >> y;int d_list[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};if (y % 4 == 0 & y % 100 != 0 || y % 400 == 0) d_list[2] = 29;if (m > 12 || m < 0 || d_list[m] < d) cout << "Invalid date\n";else {if (d_list[m] < d + 1) {d = 1;m++;if (m > 12) {m = 1;y++;}}cout << d << "/" << m << "/" << y;}return 0;}
